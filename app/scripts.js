03260cb39551df4f955e1a7dca05d313ffdbc114
object-ions <moshikoatia@gmail.comgit config --global user.name object-ions>
2023-06-03 10:08:24 -0700

    combine all if else statements into one statement condoition, get rid of test unnessecery variables. code is working

--- app/scripts.js
+++ app/scripts.js
@@ -1,59 +1,26 @@
-console.log('hello world');
 
 let form = document.querySelector('form');
 form.addEventListener('submit', function submitForm(e) {
   e.preventDefault();
   //assign the varibles values with the target tags
-  let question1 = document.getElementById('question1');
   let question1Value = document.getElementById('question1').value;
-  let question2 = document.getElementById('question2');
   let question2Value = document.getElementById('question2').value;
-  let question3 = document.getElementById('question3');
   let question3Value = document.getElementById('question3').value;
-  let question4 = document.getElementById('question4');
   let question4Value = document.getElementById('question4').value;
-  let question5 = document.getElementById('question5');
   let question5Value = document.getElementById('question5').value;
 
-
-
+  //creat a branch to check logic
   if (question1Value <= 16) {
-    console.log('since you are ' + question1Value + ' that is too young to study. come back when you are 16')
-  } else {
-    console.log('move on the Question 2');
-  };
-
-  if (question2Value === "no") {
-    console.log('I have nothing to offer you')
-  } else {
-    console.log('move on the Question 3');
-  };
-
-  if (question3Value === "yes") {
-    console.log('congratulations you are a great candid for Python!')
-  } else {
-    console.log('move on the Question 4');
-  }
-
-  if (question4Value === "yes") {
-    console.log('congratulations you are a great candid for JavaScript!');
-  } else {
-    console.log('move on the Question 5');
-  }
-
-  if (question5Value === "percious") {
+    console.log('since you are ' + question1Value + ' that is too young to study. come back when you are 16');
+  } else if (question2Value === 'no') {
+    console.log('I have nothing to offer you');
+  } else if (question3Value === 'yes') {
+    console.log('congratulations you are a great candid for Python!');
+  } else if (question4Value === 'yes') {
+    console.log('congratulations you are a great candid for JavaScript!')
+  } else if (question5Value === 'precious') {
     console.log('congratulations you are a great candid for Ruby');
   } else {
     console.log('congratulations you are a great candid for C#');
-  }
-  console.log(question1);
-  console.log(question1Value);
-  console.log(question2);
-  console.log(question2Value);
-  console.log(question3);
-  console.log(question3Value);
-  console.log(question4);
-  console.log(question4Value);
-  console.log(question5);
-  console.log(question5Value);
+  };
 });
