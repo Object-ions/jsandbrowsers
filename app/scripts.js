10a8cde3bbdf683b5dca58232572a8257e29e57c
object-ions <moshikoatia@gmail.comgit config --global user.name object-ions>
2023-06-02 17:41:36 -0700

    check different logic

--- app/scripts.js
+++ app/scripts.js
@@ -7,16 +7,50 @@ form.addEventListener('submit', function submitForm(e) {
   let question1 = document.getElementById('question1');
   let question1Value = document.getElementById('question1').value;
 
+  let question2 = document.getElementById('question2');
+  let question2Value = document.getElementById('question2').value;
+
+  let question3 = document.getElementById('question3');
+  let question3Value = document.getElementById('question3').value;
+
+  let question4 = document.getElementById('question4');
+  let question4Value = document.getElementById('question4').value;
+
+  let question5 = document.getElementById('question5');
+  let question5Value = document.getElementById('question5').value;
+
   if (question1Value <= 16) {
     console.log('since you are ' + question1Value + 'that is too young to study. come back when you are 16')
-  } else if {
-
+  } else {
+    console.log('move on the Question 2');
   }
+
+  if (question2Value === "no") {
+    console.log('I have nothing to offer you')
+  } else {
+    console.log('move on the Question 3');
+  }
+
+  if (question3Value === "yes") {
+    console.log('congratulations you are a great candid for Python!')
+  } else {
+    console.log('move on the Question 4');
+  }
+
+  if (question4Value === "yes") {
+    console.log('congratulations you are a great candid for JavaScript!');
+  } else {
+    console.log('move on the Question 5');
+  }
+
+  if (question5Value === "percious") {
+    console.log('congratulations you are a great candid for Ruby');
+  } else {
+    console.log('congratulations you are a great candid for C#');
+  }
+
 
   //show the valu in the console
   console.log(question1);
   console.log(question1Value);
-  // console.log(question3);
-  // console.log(question4);
-  // console.log(question5);
 });
