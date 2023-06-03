53e535ade9e06a8de52cdda5736ed608f1ae8838
object-ions <moshikoatia@gmail.comgit config --global user.name object-ions>
2023-06-03 10:38:49 -0700

    add elements in the html (span, div) add class .hidden in css and add js lines that will assign the user input into the #answer-display. more additions to the app

--- app/scripts.js
+++ app/scripts.js
@@ -3,24 +3,32 @@ let form = document.querySelector('form');
 form.addEventListener('submit', function submitForm(e) {
   e.preventDefault();
   //assign the varibles values with the target tags
-  let question1Value = document.getElementById('question1').value;
-  let question2Value = document.getElementById('question2').value;
-  let question3Value = document.getElementById('question3').value;
-  let question4Value = document.getElementById('question4').value;
-  let question5Value = document.getElementById('question5').value;
+  let question1 = document.getElementById('question1').value;
+  let question2 = document.getElementById('question2').value;
+  let question3 = document.getElementById('question3').value;
+  let question4 = document.getElementById('question4').value;
+  let question5 = document.getElementById('question5').value;
+  let display = document.getElementById('answer-display')
 
   //creat a branch to check logic
-  if (question1Value <= 16) {
-    console.log('since you are ' + question1Value + ' that is too young to study. come back when you are 16');
-  } else if (question2Value === 'no') {
+  if (question1 <= 16) {
+    console.log('since you are ' + question1 + ' that is too young to study. come back when you are 16');
+  } else if (question2 === 'no') {
     console.log('I have nothing to offer you');
-  } else if (question3Value === 'yes') {
+  } else if (question3 === 'yes') {
     console.log('congratulations you are a great candid for Python!');
-  } else if (question4Value === 'yes') {
+  } else if (question4 === 'yes') {
     console.log('congratulations you are a great candid for JavaScript!')
-  } else if (question5Value === 'precious') {
+  } else if (question5 === 'precious') {
     console.log('congratulations you are a great candid for Ruby');
   } else {
     console.log('congratulations you are a great candid for C#');
   };
+
+  //take the value from the user input and change the text (span) accoring to the value
+
+  document.getElementById('q1Span').innerText = question1;
+
+  display.removeAttribute('class')
+
 });
